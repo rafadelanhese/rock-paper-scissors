0697f002dffee1018a265a8d5ba5e85c09ebb37f
Rafael Delanhese <rdmmail-1@yahoo.com.br>
2019-06-02 13:20:38 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rock_paper_scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
