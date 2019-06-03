1d0f9d8e4d414f86ea2dde469e1d79e1604c9748
Rafael Delanhese <rdmmail-1@yahoo.com.br>
2019-06-03 20:02:22 -0300

    Implementação do jogo com recursividade

--- Program.cs
+++ Program.cs
@@ -10,25 +10,30 @@ namespace rock_paper_scissors
         {
             string[][] torneio = new string[2][] { new string[] { "Armando", "P", "Dave", "S", "Richard", "R", "Michael", "S" }, new string[] { "Allen", "S", "Omer", "P", "David E.", "R", "Richard X.", "P" } };
 
-            rps_tournament_winner(torneio);
+            rps_tournament_winner(torneio, 0, 1);
         }
 
-        static string rps_game_winner(string[][] torneio, int i, int j)
+        static void rps_game_winner(string[][] torneio, int i, int j)
         {
-            if (torneio[i][j].ToString().Equals("S") && torneio[i][j + 2].ToString().Equals("P") ||
+            if (j + 2 < 8)
+            {
+                if (torneio[i][j].ToString().Equals("S") && torneio[i][j + 2].ToString().Equals("P") ||
                 torneio[i][j].ToString().Equals("R") && torneio[i][j + 2].ToString().Equals("S") ||
                 torneio[i][j].ToString().Equals("P") && torneio[i][j + 2].ToString().Equals("R"))
-                rps_game_winner(torneio, i, j + 2);
-            else
-                rps_game_winner(torneio, i + 1, j + 2);
-            return torneio[i][j - 1];
-
+                    Console.WriteLine("Vencedor do Jogo: " + torneio[i][j - 1].ToString());
+                else
+                    rps_game_winner(torneio, i, j + 2);
+            }
         }
 
-        static void rps_tournament_winner(string[][] torneio)
+        static void rps_tournament_winner(string[][] torneio, int i, int j)
         {
-            string ganhador = rps_game_winner(torneio, 0, 1);
-            Console.WriteLine(ganhador);
+            rps_game_winner(torneio, i, j);
+            if (i + 1 < 2)
+            {                
+                rps_tournament_winner(torneio, i + 1, j);
+                Console.WriteLine("Vencedor do torneio: " + torneio[i][j - 1].ToString());
+            }
         }
 
     }
