17659915d120742f9721722ea4a0f1398ca5043e
Rafael Delanhese <rdmmail-1@yahoo.com.br>
2019-06-03 21:54:51 -0300

    Implementação da interação com o usuário

--- Program.cs
+++ Program.cs
@@ -7,7 +7,59 @@ namespace rock_paper_scissors
     {
         static void Main(string[] args)
         {
-            string[][] torneio = new string[2][] { new string[] { "Armando", "P", "Dave", "S", "Richard", "R", "Michael", "S" }, new string[] { "Allen", "S", "Omer", "P", "David E.", "R", "Richard X.", "P" } };
+            string estrategia, nome_jogador;
+            int qtde_jogadores, qtde_toneios;
+            //string[][] torneio = new string[2][] { new string[] { "Armando", "P", "Dave", "S", "Richard", "R", "Michael", "S" }, new string[] { "Allen", "S", "Omer", "P", "David E.", "R", "Richard X.", "P" } };
+            string[][] torneio;
+
+            do
+            {
+                Console.WriteLine("Digite a quantidade de torneios: ");
+                qtde_toneios = int.Parse(Console.ReadLine());
+
+                if (qtde_toneios < 1)
+                    Console.WriteLine("A quantidade de torneios tem que maior ou igual que 1");
+            } while (qtde_toneios < 1);
+
+            do
+            {
+                Console.WriteLine("Digite a quantidade de jogadores: ");
+                qtde_jogadores = int.Parse(Console.ReadLine());
+
+                if (qtde_jogadores < 2)
+                    Console.WriteLine("WrongNumberOfPlayersError");
+            } while (qtde_jogadores < 2);
+
+            //DEFINE O TAMANHO DO ARRAY PELA QUANTIDADE DE TORNEIOS
+            torneio = new string[qtde_toneios][];            
+            for (int i = 0; i < qtde_toneios; i++)
+            {
+
+                string[] jogadores = new string[qtde_jogadores * 2];
+
+                for (int j = 0; j < qtde_jogadores * 2; j++)
+                {
+                    Console.WriteLine("TORNEIO {0} - Digite o nome do jogador [{1}]: ", i, j);
+                    nome_jogador = Console.ReadLine();
+
+                    do
+                    {
+                        Console.WriteLine("Digite a estratégia do jogador [{0}]: ",j);
+                        estrategia = Console.ReadLine();
+                        estrategia = estrategia.ToUpper();
+
+                        if (!estrategia.Equals("S") && !estrategia.Equals("R") && !estrategia.Equals("P"))
+                            Console.WriteLine("NoSuchStrategyError");
+                        else
+                        {
+                            jogadores[j] = nome_jogador;                            
+                            jogadores[++j] = estrategia;
+                        }
+                    } while (!estrategia.Equals("S") && !estrategia.Equals("R") && !estrategia.Equals("P"));
+                }
+
+                torneio[i] = jogadores;
+            }
 
             rps_tournament_winner(torneio, 0, 1);
         }
@@ -26,14 +78,12 @@ namespace rock_paper_scissors
         }
 
         static void rps_tournament_winner(string[][] torneio, int i, int j)
-        {   
-            Console.WriteLine(torneio.Length.ToString());
-             Console.WriteLine(torneio[i].Length.ToString());
-            rps_game_winner(torneio, i, j);  
+        {
+            rps_game_winner(torneio, i, j);
             if (i + 1 < torneio.Length)
-            {                
-                rps_tournament_winner(torneio, i + 1, j);                                              
-            }                              
+            {
+                rps_tournament_winner(torneio, i + 1, j);
+            }
         }
 
     }
